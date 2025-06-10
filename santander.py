29924884aedce7d634ab9c5edf30ff8a78aa3466
juanmurta <juanmurta@gmail.com>
2025-06-10 19:44:55 -0300

    Melhorando o codigo

--- santander.py
+++ santander.py
@@ -18,30 +18,39 @@ while True:
     opcao = input(menu)
 
     if opcao == 'd':
-        valor_depositado = int(input('Digite o valor do deposito: '))
-        saldo += valor_depositado
-        print(f'Saldo atual: R$ {saldo:.2f}')
+        valor_depositado = float(input('Digite o valor do deposito: '))
+        if valor_depositado > 0:
+            saldo += valor_depositado
+            extrato += f'Depósito: R$ {valor_depositado:.2f}\n'
+            print(f'Saldo atual: R$ {saldo:.2f}')
+        else:
+            print('Informe um valor válido.')
 
     elif opcao == 's':
-        valor_sacado = int(input('Digite o valor do saque: '))
-        if saldo >= valor_sacado:
-            if valor_sacado <= limite:
-                if numero_saques < LIMITE_SAQUES:
-                    saldo -= valor_sacado
-                    numero_saques += 1
-                    print(f'Saldo atual: R$ {saldo:.2f}')
+        valor_sacado = float(input('Digite o valor do saque: '))
+        if valor_sacado > 0:
+            if saldo >= valor_sacado:
+                if valor_sacado <= limite:
+                    if numero_saques < LIMITE_SAQUES:
+                        saldo -= valor_sacado
+                        numero_saques += 1
+                        extrato += f'Sacado: R$ {valor_sacado:.2f}\n'
+                        print(f'Saldo atual: R$ {saldo:.2f}')
+                    else:
+                        print('Você ultrapassou a quantidade de 3 saques diários.')
                 else:
-                    print('Você ultrapassou a quantidade de 3 saques diários.')
+                    print('O valor do saque ultrapassou o limite por saque.')
             else:
-                print('O valor do saque ultrapassou o limite por saque.')
+                print('Saldo insuficiente.')
         else:
-            print('Saldo insuficiente.')
+            print('Informe um valor válido.')
 
     elif opcao == 'e':
-        print(f'''
-        Extrato
-        O seu saldo atual é de R$ {saldo:.2f} e você tem {LIMITE_SAQUES - numero_saques} saques disponíveis hoje..
-        ''')
+        print(30 * '#' + 'Extrato' + 30 * '#')
+        print('Não existe movimentações.' if not extrato else extrato)
+        print(f'O seu saldo atual é de R$ {saldo:.2f} e você tem {LIMITE_SAQUES - numero_saques} saque(s) restante hoje'
+              f'hoje.')
+        print(67 * '#')
 
     elif opcao == 'q':
         break
